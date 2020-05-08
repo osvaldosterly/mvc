4eb3ab8aef7da3c634bbdc326d2270782973a341
Valdo <im@osvaldopolii.com>
2020-05-08 13:56:06 +0800

    Added bootstrap 4 components

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,4 @@
-
-    <h2>About Index</h2>
+<div class="container">
+    <h2 class="mt-5">About Index</h2>
     <p>Hello var1 <b><?= $data['var1']; ?></b> and var2 <b><?= $data['var2']; ?></b> and var3 <b><?= $data['var3'] ?></b></p>
+</div>
