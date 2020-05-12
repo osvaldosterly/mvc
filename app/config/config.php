05cc0cacb97e675ae96a535fba77e407f35d27f6
Valdo <im@osvaldopolii.com>
2020-05-12 16:59:41 +0800

    Added database wrapper

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('BASEURL', 'http://localhost:8888/mvc/public');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'mvc');
--- app/models/User_model.php
+++ app/models/User_model.php
@@ -1,25 +1,15 @@
 <?php
 
 class User_model {
-
-    private $dbh; //database handler
-    private $stmt;
+    private $table = 'user';
+    private $db;
 
     public function __construct(){
-        //data source name
-        $dsn = 'mysql:host=localhost;dbname=mvc';
-
-        try{
-            $this->dbh = new PDO($dsn, 'root', 'root');
-        } catch(PDOException $e){
-            die($e->getMessage());
-        }
+        $this->db = new Database();
     }
 
-
     public function getUser(){
-        $this->stmt = $this->dbh->prepare('SELECT * FROM user');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
     }
 }
